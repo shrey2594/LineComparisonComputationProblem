ac28d138b0d5084d47254c2636668e5f54dd505b
shrey2594 <shrey2510@gmail.com>
2022-02-21 16:58:21 +0530

    Check Equality between lines

---
+++ CheckEquality.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputationProgram
+{
+    public class CheckEquality
+    {
+        public static void LineEqualityCheck(double length1,double length2)
+        {
+            if(length1.Equals(length2))
+            {
+                Console.WriteLine("Line1 is equal to Line2");
+            }
+            else
+            {
+                Console.WriteLine("Line1 is not equal to Line2");
+            }
+        }
+    }
+}

85c46479449835d38712c9efd8bae1f30b364b6e
shrey2594 <shrey2510@gmail.com>
2022-02-21 17:20:23 +0530

    Length comparison between lines

---
+++ LengthComparison.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputationProgram
+{
+    public class LengthComparison
+    {
+        public static void Comparison(double Length1,double Length2)
+        {
+            if (Length1 < Length2)
+            {
+                Console.WriteLine("Line1 is shorter than Line2");
+            }
+            else
+            {
+                Console.WriteLine("Line2 is shorter than Line1");
+            }
+        }
+    }
+}

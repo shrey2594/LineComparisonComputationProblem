0f9edecebbca28958489f7099808add03fe494b3
shrey2594 <shrey2510@gmail.com>
2022-02-21 16:09:24 +0530

    Line Comparison Solution

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonComputationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program.");
+        }
+    }
+
+}
